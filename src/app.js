d0d71c3ac283132c52f05717c3509b719a5c144e
zhangYing <zy49666@ly.com>
2018-09-14 11:17:51 +0800

    init project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var ip = require('ip');
+// 托管静态文件
+app.use(express.static('../vueClient/my-project/dist/'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  console.log('tt');
+});
+
+var server = app.listen(3000, ip.address(), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
