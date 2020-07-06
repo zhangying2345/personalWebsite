61128d122fba8846ffe7cd6a36b7a78c096ea42f
zhangying <yingzhang@siemens.com>
2020-07-06 17:47:20 +0800

    oAuth2.0 test

---
+++ vueClient/client/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+     <a href='https://github.com/login/oauth/authorize?client_id=Iv1.38662f7ecc0b2882'> 使用 GitHub 账号登录 </a>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
